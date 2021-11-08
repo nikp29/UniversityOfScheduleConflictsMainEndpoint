74481b3f6ef8449da30eb9378d40ee38d0baf269
Nikhil Patel <nhpatel@usc.edu>
2021-11-07 21:33:52 -0800

    setup for heroku deploy

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 import express from "express";
-// const routesApi = require('./api/routes/index');
+
 import bodyParser from "body-parser";
 import routes from "./routes.js";
-
-import swaggerJsdoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-// import swaggerFile from "./swagger_output.json";
 
 const server = express();
 const port = process.env.PORT || 3000;
@@ -13,30 +9,6 @@ server.use(bodyParser.json());
 server.use(express.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use("/api", routes);
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "University of Scehduling Conflicts Express API with Swagger",
-            version: "0.1.0",
-            description:
-                "This is a simple CRUD API application made with Express and documented with Swagger",
-            license: {
-                name: "MIT",
-                url: "https://spdx.org/licenses/MIT.html",
-            },
-        },
-        servers: [
-            {
-                url: "http://localhost:3000",
-            },
-        ],
-    },
-    apis: ["./routes.js"],
-};
-
-const specs = swaggerJsdoc(options);
-server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 server.listen(port, () => console.log("server started on port " + port));
 // app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
