cea5ef7486b4fa68f1ecac63b1996e8a32cffb6e
Isaac Gerstmann <Isaac Gerstmann>
2021-11-29 13:41:28 -0800

    Request validation for merge endpoint

--- merge/src/main/java/com/uofschedulingconflicts/merge/ScheduleController.java
+++ merge/src/main/java/com/uofschedulingconflicts/merge/ScheduleController.java
@@ -1,6 +1,11 @@
 package com.uofschedulingconflicts.merge;
 
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
@@ -14,6 +19,11 @@ public class ScheduleController {
 
         int[][]schedules = new int[schedulesAL.size()][];
         for(int i = 0; i < schedules.length; i++){
+            if (schedulesAL.get(i).size() != 13 * 4 *7){
+                System.out.println(schedulesAL.get(i)   .size() + "  " + (13 * 4* 7));
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Incorrect Array Size");
+            }
+
             schedules[i] = new int[schedulesAL.get(i).size()];
             for (int j = 0; j < schedules[i].length; j++){
                 schedules[i][j] = schedulesAL.get(i).get(j);
