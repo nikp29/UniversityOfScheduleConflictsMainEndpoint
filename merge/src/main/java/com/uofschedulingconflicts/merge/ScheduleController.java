c7eb0af64ba024ff8d8bfa0361b061647f6b4bb2
Scott Susanto <scottsus@usc.edu>
2021-11-30 17:53:37 -0800

    Update array size

--- merge/src/main/java/com/uofschedulingconflicts/merge/ScheduleController.java
+++ merge/src/main/java/com/uofschedulingconflicts/merge/ScheduleController.java
@@ -11,20 +11,20 @@ import java.util.*;
 
 @RestController
 public class ScheduleController {
-    
+
     @RequestMapping("/merge")
-    public @ResponseBody Map<String, Object> mergeSchedules (@RequestBody Map<String, Object> input){
+    public @ResponseBody Map<String, Object> mergeSchedules(@RequestBody Map<String, Object> input) {
         ArrayList<ArrayList<Integer>> schedulesAL = (ArrayList<ArrayList<Integer>>) input.get("schedules");
         int eventLen = (int) input.get("eventLen");
 
-        int[][]schedules = new int[schedulesAL.size()][];
-        for(int i = 0; i < schedules.length; i++){
-            if (schedulesAL.get(i).size() != 13 * 4 *7){
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Incorrect Array Size");
+        int[][] schedules = new int[schedulesAL.size()][];
+        for (int i = 0; i < schedules.length; i++) {
+            if (schedulesAL.get(i).size() != 7 * 13) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect Array Size");
             }
 
             schedules[i] = new int[schedulesAL.get(i).size()];
-            for (int j = 0; j < schedules[i].length; j++){
+            for (int j = 0; j < schedules[i].length; j++) {
                 if (schedulesAL.get(i).get(j) != 0 && schedulesAL.get(i).get(j) != 1) {
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Array Entry");
                 }
@@ -32,13 +32,12 @@ public class ScheduleController {
             }
         }
 
-        Scheduler s = new Scheduler(13 * 4 * 7);
+        Scheduler s = new Scheduler(7 * 13);
         int[] merged = s.searchForTimes(schedules, eventLen);
 
         Map<String, Object> output = new HashMap<String, Object>();
         output.put("mergedSchedule", merged);
         return output;
     }
-
 
 }
