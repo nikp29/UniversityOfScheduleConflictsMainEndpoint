a0824e61794295fd1fd1962a97d2e89846c43e02
Nikhil Patel <nhpatel@usc.edu>
2021-12-06 20:03:41 -0800

    fixed cors for java as well

--- merge/src/main/java/com/uofschedulingconflicts/merge/ScheduleController.java
+++ merge/src/main/java/com/uofschedulingconflicts/merge/ScheduleController.java
@@ -13,6 +13,7 @@ import java.util.*;
 public class ScheduleController {
 
     @RequestMapping("/merge")
+    @CrossOrigin(origins = "http://localhost:3000")
     public @ResponseBody Map<String, Object> mergeSchedules(@RequestBody Map<String, Object> input) {
         ArrayList<ArrayList<Integer>> schedulesAL = (ArrayList<ArrayList<Integer>>) input.get("schedules");
         int eventLen = (int) input.get("eventLen");
